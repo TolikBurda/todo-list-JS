2e2a4b8b74628758aebc4956530b300a703900b6
TolikBurda <tolik.burda@gmail.com>
2019-11-06 14:05:58 +0200

    project start

---
+++ todoList.js
@@ -0,0 +1,248 @@
+const uuid =()=>([1e7]+-1e3+-4e3+-8e3+-1e11)
+    .replace(/[018]/g,c=>(c^crypto
+    .getRandomValues(new Uint8Array(1))[0]&15 >> c/4)
+    .toString(16));
+
+class Component {
+    constructor() {
+        this.element = null;
+    }
+
+    render(){
+
+    }
+}
+
+class TodoAppComponent extends Component{
+    constructor() {
+        super();
+        this.pubsub = new PubSub();
+        this.element = document.createElement('div');
+        this.form = new TodoFormComponent(this.pubsub);
+        this.todoList = new TodoListComponent(this.pubsub);
+        this.todoService = new TodoService(this.pubsub);
+        this.title = 'My 2Do';
+        document.getElementsByTagName('header')[0].innerHTML = this.title;
+
+        this.render();
+
+        // this.pubsub.subscribe('create', this, this.createTodo); //from form
+        // this.pubsub.subscribe('delete', this, this.deleteTodo); //from todoItem
+        // this.pubsub.subscribe('toggle', this, this.toggleTodo); //from todoItem
+    }
+    render(){
+        this.element.append(this.form.element);
+        this.element.append(this.todoList.element)
+        document.body.append(this.element);
+        this.handleEvent();
+    }
+    handleEvent() {
+        let form = document.getElementsByClassName('todo-form')[0].addEventListener('submit', (e)=>{
+            e.preventDefault();
+            this.createTodo(e.target.firstChild.value);
+            e.target.firstChild.value = '';
+        });
+    }
+    createTodo(title) {
+        this.todoService.createTodo(title);
+        
+        this.pubsub.fireEvent('create', this.todoService.getTodos());
+
+        // this.todoList.show();
+    }
+    deleteTodo() {}
+    toggleTodo() {}
+
+}
+
+class TodoService {
+    constructor(pubsub) {
+        this.pubsub = pubsub;
+        this.todos = [];
+
+        // this.pubsub.subscribe('create', this, this.createTodo);
+    }
+
+    createTodo(title){
+        let todo = new TodoItemComponent(title);
+        this.todos.push(todo);
+        console.log('created', this.todos);
+    }
+
+    deleteTodo(id){
+        let todoToDel = this.todos.findIndex(todo => todo.id === id);
+        this.todos.splice(todoToDel, 1);
+    }
+    
+    toggleTodo(id){
+        let todoToToggle = this.todos.find(todo => todo.id === id);
+        console.log(id, 'todoList Class');
+        todoToToggle.completed = !todoToToggle.completed;
+    }
+
+    getTodos() {
+        return this.todos;
+    }
+
+}
+
+class TodoFormComponent extends Component {
+    constructor(pubsub) {
+        super();
+        this.pubsub = pubsub;
+        // this.submitBtn = null;
+        this.render();
+        // this.formListener();
+    }
+
+    // formListener(){
+    //     // let form = document.getElementsByClassName('todo-form')[0].addEventListener('submit', (e)=>{
+    //     //     e.preventDefault();
+    //     //     // this.createTodo(e.target.firstChild.value);
+    //     //     e.target.firstChild.value = ''
+    //     // })
+    //     // let form = document.getElementsByClassName('todo-form');
+    //     // // let form = document.getElementById('submitBtm');
+    // }
+
+    render() {
+        let form = document.createElement('form');
+        form.className = 'todo-form';
+        let input = document.createElement('input');
+        input.type = 'text';
+        input.placeholder = 'What do we have to do?'
+        form.append(input);
+
+        let submitButton = document.createElement('button');
+        submitButton.type = 'submit';
+        submitButton.id = 'submitBtm'
+        submitButton.innerText = 'Add';
+        // this.submitBtn = submitButton;
+        form.append(submitButton);
+        this.element = form;
+    }
+}
+
+class TodoListComponent extends Component{
+    constructor(pubsub) {
+        super();
+        this.element = document.createElement('div');
+        this.pubsub = pubsub;
+        this.todoList = null;
+        this.render();
+        this.pubsub.subscribe('create', this, this.show);
+    }
+    render(){
+        if(this.todoList){
+            this.todoList.forEach(todoItem => {      
+                this.element.append(todoItem.element)
+            })
+        }
+    }
+    show(data){
+        this.todoList = data;
+        this.render();
+    }
+}
+
+class TodoItemComponent extends Component{/////////////////stop here
+    constructor(title) {
+        super();
+        this.id = uuid();
+        this.title = title;
+        this.completed = false;
+        this.render(this.title);
+        console.log(`hello, im your todo, my task is "${title}"`);
+        
+    }
+    render(title) {
+        let arrOfHtmlElements = [];
+
+        let section = document.createElement('section');
+        section.className = 'todo-list';
+
+        let todoItemDiv = document.createElement('div');
+        todoItemDiv.className = 'todo-item';
+        // todoItemDiv.contentEditable = true;
+        todoItemDiv.className += this.completed ? ' completed' : '';
+
+
+        let checkboxButton = document.createElement('button');
+        checkboxButton.classList.add('checkbox', 'icon' );
+
+        let i = document.createElement('i');
+        i.className = 'material-icons';
+        i.innerText = this.completed ? 'check_box' : 'check_box_outline_blank';
+        checkboxButton.prepend(i);
+        // this.checkBoxButton = checkboxButton;
+    
+        let span = document.createElement('span');
+        span.className = 'title';
+        span.innerText = title;
+        // this.span = span;
+
+        let actionsDiv = document.createElement('div');
+        actionsDiv.className = 'actions';
+
+        let deleteButton = document.createElement('button');
+        deleteButton.classList.add('delete', 'icon');
+
+        let iDel = document.createElement('i');
+        iDel.className = 'material-icons';
+        iDel.innerText = 'delete';
+        deleteButton.append(iDel);
+
+        actionsDiv.append(deleteButton);
+        // this.delButton = deleteButton;
+        
+        arrOfHtmlElements.push(checkboxButton);
+        arrOfHtmlElements.push(span);
+        arrOfHtmlElements.push(actionsDiv);
+
+        for(let n = 0; n < arrOfHtmlElements.length; n++){
+            todoItemDiv.append(arrOfHtmlElements[n]);
+            section.append(todoItemDiv);
+        }
+        this.element = section;      
+    }
+    toggle() {
+        // this.completed = !this.completed;
+        // console.log('todo class');
+    }
+    delete() {
+
+    }
+
+}
+
+//-------------------------------------------------------------------------------------------
+class Subscription {
+    constructor(event, obj, method) {
+        this.event = event;
+        this.obj = obj;
+        this.method = method;
+    }
+}
+
+class PubSub {
+    constructor() {
+        this.subscriptions = [];
+    }
+
+    subscribe(event, obj, method) {
+        var sub = new Subscription(event, obj, method);
+        this.subscriptions.push(sub);
+    }
+
+    fireEvent(event, data) {
+        for (let i = 0; i < this.subscriptions.length; i++) {
+            const sub = this.subscriptions[i];
+            if (sub.event == event) {
+                sub.method.call(sub.obj, data);
+            }
+        }
+    }
+}
+
+let todo = new TodoAppComponent();
+// document.body.append(todo.element);
